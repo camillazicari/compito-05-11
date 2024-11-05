a1a86ffb9f280e592665580a4b3efb3a5ad441d4
camillazicari <camyzicari01@gmail.com>
2024-11-05 16:09:37 +0100

    es 10

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,7 +121,6 @@ let variabile3 = 1400;
     }
  } */
 
-
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
@@ -139,7 +138,7 @@ else {
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* SCRIVI QUI LA TUA RISPOSTA 
 let number = 18;
 let resto = number % 2;
 
@@ -147,11 +146,10 @@ if (resto === 0 ) {
     console.log('il numero è pari');
 }
 else {console.log('il numero è dispari');
-    
-}
+} */
 
 /* ESERCIZIO 10
-  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza. 
   let val = 7
   if (val < 10) {
       console.log("Meno di 10");
@@ -159,10 +157,19 @@ else {console.log('il numero è dispari');
       console.log("Meno di 5");
     } else {
       console.log("Uguale a 10 o maggiore");
-    }
-*/
+    } */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* SCRIVI QUI LA TUA RISPOSTA 
+let val = 7;
+if (val < 5) {
+  console.log("Meno di 5");
+}
+else if (val < 10) {
+  console.log("Meno di 10");
+}  
+else {
+  console.log("Uguale a 10 o maggiore");
+} */
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
@@ -172,29 +179,29 @@ const me = {
     lastName: 'Doe',
     skills: ['javascript', 'html', 'css'],
   } */
-  
-  /* SCRIVI QUI LA TUA RISPOSTA */
-  
-  /* ESERCIZIO 12
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 12
     Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
   */
-  
-  /* SCRIVI QUI LA TUA RISPOSTA */
-  
-  /* ESERCIZIO 13
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 13
     Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
   */
-  
-  /* SCRIVI QUI LA TUA RISPOSTA */
-  
-  /* ESERCIZIO 14
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 14
     Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
   */
-  
-  /* SCRIVI QUI LA TUA RISPOSTA */
-  
-  /* ESERCIZIO 15
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 15
     Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
   */
-  
-  /* SCRIVI QUI LA TUA RISPOSTA */
+
+/* SCRIVI QUI LA TUA RISPOSTA */
